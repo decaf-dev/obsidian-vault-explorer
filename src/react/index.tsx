b483f785663288d9a47fafb35d04b37183de439e
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2024-04-15 22:43:49 -0600

    feat: add business logic for timestamp filtering

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -218,20 +218,31 @@ export default function ReactApp() {
 	);
 
 	const filteredData: MarkdownFileData[] = sortedMarkdownFiles
-		// .filter((file) => {
-		// 	if (onlyModifiedToday) {
-		// 		const midnightToday = moment().startOf("day").valueOf();
-		// 		return file.stat.mtime > midnightToday;
-		// 	}
-		// 	return true;
-		// })
-		// .filter((file) => {
-		// 	if (onlyCreatedToday) {
-		// 		const midnightToday = moment().startOf("day").valueOf();
-		// 		return file.stat.ctime > midnightToday;
-		// 	}
-		// 	return true;
-		// })
+		.filter((file) => {
+			const midnightToday = moment().startOf("day").valueOf();
+			const midnightThisWeek = moment().startOf("week").valueOf();
+
+			//This is the Sunday the previous week
+			const midnightLastWeek = moment()
+				.subtract(1, "weeks")
+				.startOf("week")
+				.valueOf();
+
+			if (timestampFilter === "modified-this-week") {
+				return file.stat.mtime > midnightThisWeek;
+			} else if (timestampFilter === "created-this-week") {
+				return file.stat.ctime > midnightThisWeek;
+			} else if (timestampFilter === "modified-2-weeks") {
+				return file.stat.mtime > midnightLastWeek;
+			} else if (timestampFilter === "created-2-weeks") {
+				return file.stat.ctime > midnightLastWeek;
+			} else if (timestampFilter === "modified-today") {
+				return file.stat.mtime > midnightToday;
+			} else if (timestampFilter === "created-today") {
+				return file.stat.ctime > midnightToday;
+			}
+			return true;
+		})
 		.map((file) => {
 			const frontmatter = app.metadataCache.getFileCache(
 				file as TFile
