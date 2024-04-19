26fac8a746609163906b18ea86cf2d4001367a24
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2024-04-19 01:24:00 -0600

    refactor: remove into shared components and services folder

--- src/svelte/app/services/filters/property-filter.ts
+++ src/svelte/app/services/filters/property-filter.ts
@@ -1,6 +1,6 @@
 import { App, TFile } from "obsidian";
 import { PropertyFilterGroup } from "src/types";
-import { matchesPropertyFilter } from "../../../../react/components/app/utils";
+import { FilterCondition, TextFilterCondition } from "src/types";
 
 //Tests
 //Group is enabled/disabled
@@ -58,3 +58,40 @@ export const filterByProperty = (app: App, file: TFile, groups: PropertyFilterGr
 	});
 	return isValid;
 }
+
+const matchesPropertyFilter = (
+	condition: FilterCondition,
+	propertyValue: string,
+	compare: string,
+	shouldMatchIfNull: boolean
+): boolean => {
+	propertyValue = propertyValue.toLowerCase().trim();
+	compare = compare.toLowerCase().trim();
+
+	switch (condition) {
+		case TextFilterCondition.IS:
+			if (compare === "") return shouldMatchIfNull;
+			return propertyValue === compare;
+		case TextFilterCondition.IS_NOT:
+			if (compare === "") return shouldMatchIfNull;
+			return propertyValue !== compare;
+		case TextFilterCondition.CONTAINS:
+			if (compare === "") return shouldMatchIfNull;
+			return propertyValue.includes(compare);
+		case TextFilterCondition.DOES_NOT_CONTAIN:
+			if (compare === "") return shouldMatchIfNull;
+			return !propertyValue.includes(compare);
+		case TextFilterCondition.STARTS_WITH:
+			if (compare === "") return shouldMatchIfNull;
+			return propertyValue.startsWith(compare);
+		case TextFilterCondition.ENDS_WITH:
+			if (compare === "") return shouldMatchIfNull;
+			return propertyValue.endsWith(compare);
+		case TextFilterCondition.IS_EMPTY:
+			return propertyValue === "";
+		case TextFilterCondition.IS_NOT_EMPTY:
+			return propertyValue !== "";
+		default:
+			throw new Error("Filter condition not yet supported");
+	}
+};
