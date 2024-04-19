21b8c1da436e0683a376b248c3afa72b5c757574
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2024-04-19 01:23:50 -0600

    refactor: remove app folder

--- src/react/components/app/types.ts
+++
@@ -1,9 +0,0 @@
-export interface MarkdownFileRenderData {
-	name: string;
-	path: string;
-	tags: string[] | null;
-	source: string | null;
-	favorite: string | null;
-	url: string | null;
-	status: string | null;
-}
