10843defa321d91037125d5ad3d9c0ca2a62da04
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2024-04-20 00:53:58 -0600

    fix: fix tabs not updating (#13)

--- src/svelte/shared/components/tab-list.svelte
+++ src/svelte/shared/components/tab-list.svelte
@@ -1,1 +1,18 @@
-<div><slot /></div>
+<script>
+	import { setContext } from "svelte";
+	import { writable } from "svelte/store";
+
+	let numTabs = 0;
+	const selectedTab = writable(0); // A store to keep track of the selected tab
+
+	function registerTab() {
+		return numTabs++;
+	}
+
+	setContext("selectedTab", selectedTab);
+	setContext("registerTab", registerTab);
+</script>
+
+<div>
+	<slot />
+</div>
--- src/svelte/shared/components/tab.svelte
+++ src/svelte/shared/components/tab.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
-	export let isSelected: boolean = false;
-
-	import { createEventDispatcher } from "svelte";
+	import { createEventDispatcher, getContext } from "svelte";
 	const dispatch = createEventDispatcher();
 
+	let index: number;
+
+	const selectedTab = getContext("selectedTab") as Writable<number>;
+	const registerTab = getContext("registerTab") as () => number;
+
+	// We use onMount to ensure the index is set after the component is mounted
+	import { onMount } from "svelte";
+	import { Writable } from "svelte/store";
+	onMount(() => {
+		index = registerTab();
+	});
+
 	function handleClick(event: Event) {
+		selectedTab.set(index);
 		dispatch("click", { nativeEvent: event });
 	}
 
-	let className = "vault-explorer-tab";
-	if (isSelected) {
-		className += " vault-explorer-tab--active";
-	}
+	$: isSelected = $selectedTab === index;
+
+	$: className =
+		"vault-explorer-tab" +
+		(isSelected ? " vault-explorer-tab--active" : "");
 </script>
 
 <button class={className} on:click={handleClick}><slot /></button>
