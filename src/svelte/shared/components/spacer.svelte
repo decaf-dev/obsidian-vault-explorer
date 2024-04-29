a550ea12282a53a4231ddb976205135e31416a84
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2024-04-29 06:45:23 -0600

    fix: resolve spacer not displaying

--- src/svelte/shared/components/spacer.svelte
+++ src/svelte/shared/components/spacer.svelte
@@ -1,26 +1,39 @@
 <script lang="ts">
-	export let direction: "horizontal" | "vertical" = "vertical";
-	export let size: "" | "xs" | "sm" | "md" | "lg" = "";
+	export let direction: "horizontal" | "vertical" = "horizontal";
+	export let size: "" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl" = "";
+
+	$: size, direction, findDimesions();
 
 	let width = 0;
 	let height = 0;
-	switch (size) {
-		case "xs":
-			if (direction == "horizontal") width = 4;
-			if (direction == "vertical") height = 4;
-			break;
-		case "sm":
-			if (direction == "horizontal") width = 8;
-			if (direction == "vertical") height = 8;
-			break;
-		case "md":
-			if (direction == "horizontal") width = 16;
-			if (direction == "vertical") height = 16;
-			break;
-		case "lg":
-			if (direction == "horizontal") width = 24;
-			if (direction == "vertical") height = 24;
-			break;
+
+	function findDimesions() {
+		switch (size) {
+			case "xs":
+				if (direction == "horizontal") width = 4;
+				if (direction == "vertical") height = 4;
+				break;
+			case "sm":
+				if (direction == "horizontal") width = 8;
+				if (direction == "vertical") height = 8;
+				break;
+			case "md":
+				if (direction == "horizontal") width = 16;
+				if (direction == "vertical") height = 16;
+				break;
+			case "lg":
+				if (direction == "horizontal") width = 24;
+				if (direction == "vertical") height = 24;
+				break;
+			case "xl":
+				if (direction == "horizontal") width = 32;
+				if (direction == "vertical") height = 32;
+				break;
+			case "2xl":
+				if (direction == "horizontal") width = 48;
+				if (direction == "vertical") height = 48;
+				break;
+		}
 	}
 </script>
 
