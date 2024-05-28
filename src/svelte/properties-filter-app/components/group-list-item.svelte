334498708659edb578e8c4dc9d186a4daec2baee
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2024-05-28 07:44:21 -0600

    feat: remove drag icon

--- src/svelte/properties-filter-app/components/group-list-item.svelte
+++ src/svelte/properties-filter-app/components/group-list-item.svelte
@@ -1,26 +1,13 @@
 <script lang="ts">
-	import Flex from "src/svelte/shared/components/flex.svelte";
-	import Icon from "src/svelte/shared/components/icon.svelte";
-
 	export let id: string;
 	export let name: string;
 	export let isSelected: boolean;
-
-	let ref: HTMLElement;
 
 	import { createEventDispatcher } from "svelte";
 	const dispatch = createEventDispatcher();
 
 	function handleClick() {
 		dispatch("itemClick", { id });
-	}
-
-	function handleMouseDown(event: Event) {
-		ref.setAttribute("draggable", "true");
-		Object.defineProperty(event, "target", {
-			value: ref,
-		});
-		ref.dispatchEvent(new Event(event.type, event));
 	}
 
 	function handleDragStart(event: Event) {
@@ -35,50 +22,26 @@
 		dispatch("itemDrop", { nativeEvent: event, id });
 	}
 
-	function handleDragEnd(event: Event) {
-		dispatch("itemDragEnd", { nativeEvent: event, id });
-	}
-
 	$: className =
 		"vault-explorer-group-item" +
 		(isSelected ? " vault-explorer-group-item--active" : "");
 </script>
 
 <div
-	role="listitem"
-	bind:this={ref}
+	tabindex="0"
+	draggable="true"
+	role="button"
+	class={className}
 	on:dragstart={handleDragStart}
 	on:dragover={handleDragOver}
 	on:drop={handleDrop}
-	on:dragend={handleDragEnd}
+	on:click={handleClick}
+	on:keydown={(e) => (e.key === "Enter" || e.key === " ") && handleClick()}
 >
-	<Flex align="center">
-		<div
-			tabindex="0"
-			role="button"
-			class="vault-explorer-group-item__icon"
-			on:mousedown={handleMouseDown}
-		>
-			<Icon iconId="grip-vertical" />
-		</div>
-		<div
-			tabindex="0"
-			role="button"
-			class={className}
-			on:click={handleClick}
-			on:keydown={(e) =>
-				(e.key === "Enter" || e.key === " ") && handleClick()}
-		>
-			{name}
-		</div>
-	</Flex>
+	{name}
 </div>
 
 <style>
-	.vault-explorer-group-item__icon {
-		margin-top: 4px;
-	}
-
 	.vault-explorer-group-item {
 		display: flex;
 		width: calc(100% - 12px);
@@ -89,7 +52,7 @@
 		background-color: var(--background-modifier-hover);
 	}
 
-	.vault-explorer-group-button--active {
+	.vault-explorer-group-item--active {
 		background-color: var(--background-modifier-hover);
 	}
 </style>
