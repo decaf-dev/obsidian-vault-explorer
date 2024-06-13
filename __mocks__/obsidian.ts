69417fd623db5f816a788ea7d4199591a01c7556
DecafDev <40307803+decaf-dev@users.noreply.github.com>
2024-06-13 17:02:31 -0600

    test: add types to moment function

--- __mocks__/obsidian.ts
+++ __mocks__/obsidian.ts
@@ -1,7 +1,14 @@
+/**
+ * Matches the date format of YYYY-MM-DD
+ */
 const DATE_REGEX = /^\d{4}-\d{2}-\d{2}$/;
+
+/**
+ * Matches the date format of YYYY-MM-DDTHH:MM:SS
+ */
 const DATE_TIME_REGEX = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/;
 
-export const moment = jest.fn((date, formats, strict) => {
+export const moment = jest.fn((date: string, _formats: string[], _strict?: boolean) => {
 	return {
 		isValid: jest.fn(() => DATE_REGEX.test(date) || DATE_TIME_REGEX.test(date)),
 	};
