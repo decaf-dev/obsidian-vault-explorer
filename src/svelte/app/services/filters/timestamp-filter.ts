5901461241452ba687b2c49bb2f5a1a5a63ffbdf
DecafDev <40307803+decaf-dev@users.noreply.github.com>
2024-06-23 20:01:48 -0600

    fix: resolve crash on group click

--- src/svelte/app/services/filters/timestamp-filter.ts
+++ src/svelte/app/services/filters/timestamp-filter.ts
@@ -1,7 +1,7 @@
 import { TimestampFilterOption } from "src/types";
 
 interface FilterByTimestampParams {
-	timestampFilter: TimestampFilterOption;
+	value: TimestampFilterOption;
 	createdMillis: number;
 	modifiedMillis: number;
 	startOfTodayMillis: number;
@@ -10,24 +10,24 @@ interface FilterByTimestampParams {
 }
 
 export const filterByTimestamp = ({
-	timestampFilter,
+	value,
 	createdMillis,
 	modifiedMillis,
 	startOfTodayMillis,
 	startOfThisWeekMillis,
 	startOfLastWeekMillis,
 }: FilterByTimestampParams) => {
-	if (timestampFilter === "modified-this-week") {
+	if (value === "modified-this-week") {
 		return modifiedMillis > startOfThisWeekMillis;
-	} else if (timestampFilter === "created-this-week") {
+	} else if (value === "created-this-week") {
 		return createdMillis > startOfThisWeekMillis;
-	} else if (timestampFilter === "modified-2-weeks") {
+	} else if (value === "modified-2-weeks") {
 		return modifiedMillis > startOfLastWeekMillis;
-	} else if (timestampFilter === "created-2-weeks") {
+	} else if (value === "created-2-weeks") {
 		return createdMillis > startOfLastWeekMillis;
-	} else if (timestampFilter === "modified-today") {
+	} else if (value === "modified-today") {
 		return modifiedMillis > startOfTodayMillis;
-	} else if (timestampFilter === "created-today") {
+	} else if (value === "created-today") {
 		return createdMillis > startOfTodayMillis;
 	}
 	return true;
