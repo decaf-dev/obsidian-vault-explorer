02ec868d60fa775c7ad6308e90577ffccc55cbca
DecafDev <40307803+decaf-dev@users.noreply.github.com>
2024-07-10 13:26:56 -0600

    feat: rename to loadSocialMediaImage

---
+++ src/migrations/migrate_1_24_0.ts
@@ -0,0 +1,23 @@
+import { VaultExplorerPluginSettings } from "src/types";
+import MigrationInterface from "./migration_interface";
+import { VaultExplorerPluginSettings_1_23_2 } from "src/types/types-1.23.2";
+
+export default class Migrate_1_24_0 implements MigrationInterface {
+	migrate(data: Record<string, unknown>) {
+		const typedData = data as unknown as VaultExplorerPluginSettings_1_23_2;
+		const newData: VaultExplorerPluginSettings = {
+			...typedData,
+			views: {
+				...typedData.views,
+				grid: {
+					...typedData.views.grid,
+					loadSocialMediaImage:
+						typedData.views.grid.fetchSocialMediaImage,
+				},
+			},
+		};
+
+		delete (newData as any).views.grid.fetchSocialMediaImage;
+		return newData as unknown as Record<string, unknown>;
+	}
+}
