5c439e7e61d675d1e630e7764b7dce10fbf9f45c
DecafDev <40307803+decaf-dev@users.noreply.github.com>
2024-07-13 14:44:37 -0600

    feat: add file interaction style setting

--- src/migrations/migrate_1_24_0.ts
+++ src/migrations/migrate_1_24_0.ts
@@ -1,11 +1,11 @@
-import { VaultExplorerPluginSettings } from "src/types";
 import MigrationInterface from "./migration_interface";
 import { VaultExplorerPluginSettings_1_23_2 } from "src/types/types-1.23.2";
+import { VaultExplorerPluginSettings_1_24_2 } from "src/types/types-1.24.2";
 
 export default class Migrate_1_24_0 implements MigrationInterface {
 	migrate(data: Record<string, unknown>) {
 		const typedData = data as unknown as VaultExplorerPluginSettings_1_23_2;
-		const newData: VaultExplorerPluginSettings = {
+		const newData: VaultExplorerPluginSettings_1_24_2 = {
 			...typedData,
 			views: {
 				...typedData.views,
